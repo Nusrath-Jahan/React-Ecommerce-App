2b47cabf551a437df407d2a5c7cf6b761bf4b635
Nusrath <nusrathjahan0204@gmail.com>
2025-09-18 16:14:46 +0200

    added a card page

--- ecommerce-app/src/App.js
+++ ecommerce-app/src/App.js
@@ -3,40 +3,46 @@ import { Routes, Route, Link } from "react-router-dom";
 import { AppBar, Toolbar, Button, Container } from "@mui/material";
 import Home from "./pages/Home";
 import ProductDetails from "./pages/ProductDetails";
+import Card from "./pages/Card";
 
 function App() {
   return (
-  <div>
-    {/* Navbar */}
-    <AppBar position="static" sx={{backgroundColor: "#f62121ff"}}>
-      <Toolbar>
-<Button component={Link} to="/" sx={{ color: "white"}}>Home</Button>
-      </Toolbar>
+    <div>
+      {/* Navbar */}
+      <AppBar position="static" sx={{ backgroundColor: "#f62121ff" }}>
+        <Toolbar>
+          <Button component={Link} to="/" sx={{ color: "white" }}>
+            Home
+          </Button>
+          <Button color="inherit" component={Link} to="/card">
+            Card
+          </Button>
+        </Toolbar>
       </AppBar>
 
-{/* Pages */}
-<Container sx={{mt: 3}}>
-  <Routes>
-    <Route path="/" element={<Home /> } />
-    <Route path="/product/:id" element={<ProductDetails />} />
-  </Routes>
-</Container>
-
-  </div>
-  )
+      {/* Pages */}
+      <Container sx={{ mt: 3 }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/card" element={<Card />} />
+        </Routes>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
 
+{
+  /* <AppBar> creates a top navigation bar for the application using Material-UI.
+It typically contains branding, navigation links, or action buttons. */
+}
 
-
-
-
- {/* <AppBar> creates a top navigation bar for the application using Material-UI.
-It typically contains branding, navigation links, or action buttons. */}
-
-{/* <Toolbar> provides a flexible container for grouping and aligning navigation elements, buttons, titles, or icons within the app bar. 
-<Toolbar> helps with consistent spacing and layout of items in the top navigation bar.   */}
+{
+  /* <Toolbar> provides a flexible container for grouping and aligning navigation elements, buttons, titles, or icons within the app bar. 
+<Toolbar> helps with consistent spacing and layout of items in the top navigation bar.   */
+}
 
 // mt stands for margin-top
 //   sx is a prop in MUI that allows you to apply custom styles using a JavaScript object.
---
+++ ecommerce-app/src/pages/Card.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Typography, Button, Card, CardContent } from "@mui/material";
+import { removeFromCart, increaseQty, decreaseQty } from "../redux/cartSlice";
+
+function Cart() {
+  const cart = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+
+  const total = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
+
+  if (cart.length === 0) {
+    return <Typography variant="h5">Your cart is empty</Typography>;
+  }
+  return (
+    <div>
+      <Typography variant="h4">Your Cart</Typography>
+      {cart.map((item) => (
+        <Card key={item.id} sx={{ mt: 2 }}>
+          <CardContent>
+            <Typography variant="h6">{item.title}</Typography>
+            <Typography>
+              ${item.price} * {item.quantity}
+            </Typography>
+            <Button onClick={() => dispatch(increaseQty(item.id))}>+</Button>
+            <Button onClick={() => dispatch(decreaseQty(item.id))}>-</Button>
+            <Button
+              color="error"
+              onClick={() => dispatch(removeFromCart(item.id))}
+            >
+              Remove
+            </Button>
+          </CardContent>
+        </Card>
+      ))}
+      <Typography>Total: ${total.toFixed(2)}</Typography>
+    </div>
+  );
+}
+
+export default Cart;
