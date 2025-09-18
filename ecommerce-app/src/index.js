2be864f4d40060ef78fb5d66aef286056d478709
Nusrath <nusrathjahan0204@gmail.com>
2025-09-18 15:30:20 +0200

    added redux folder

--- ecommerce-app/src/index.js
+++ ecommerce-app/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  <Provider store={store}>
   <BrowserRouter>
   <App/>
   </BrowserRouter>
+  </Provider>
 )
 
 // <BrowserRouter> enables client-side routing using the HTML5 history API.
 // <BrowserRouter> allows navigation between components without full page reloads.
+// <Provider> makes the Redux store available to the entire app, allowing components to access and interact with the global state. 
